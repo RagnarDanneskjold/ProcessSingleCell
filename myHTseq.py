32578d1ca96a379131666b8b7840dd51ecbd50c0
Aparna Rajpurkar <aparna.arr@gmail.com>
2017-03-09 13:57:39 -0800

    found bug in GTF processor, going to fix

--- myHTseq.py
+++ myHTseq.py
@@ -189,12 +189,13 @@ def parseFragment(readPair):
 
 ## END FUNCTIONS ##
 
-if (len(sys.argv) < 2):
-	print("usage:", sys.argv[0], "<name-sorted bamfile> <GENCODE gtf file>")
+if (len(sys.argv) < 3):
+	print("usage:", sys.argv[0], "<list with full paths of name-sorted bamfiles> <GENCODE gtf file> <outfile name>")
 	sys.exit(1)
 
 bamfiles_list_file = sys.argv[1]
 gtffile = sys.argv[2]
+outfilename = sys.argv[3]
 
 print ("DEBUG: opening GTF file", file=sys.stderr)
 gene_elements = parseGTFFile(gtffile)
@@ -247,7 +248,6 @@ for bam in bamfiles:
 						sys.exit(1)
 
 				# get start, end, strand of the FRAGMENT
-
 				frag = parseFragment(prev_reads)
 
 				if frag == {}:
@@ -255,17 +255,8 @@ for bam in bamfiles:
 					prev_read_name = read.query_name
 					prev_reads = [read]
 					continue
-			#parsedData[chrom][strand].append({
-			#		'gene_id':gene_id,
-			#		'start':start,
-			#		'end':end,
-			#		'exons':list(),
-			#		'intronic':list(),
-			#		'reads':list()
-			#	})
 				curr_chrom = prev_reads[0].reference_id
 				curr_chrom = "chr" + str(curr_chrom)
-				#print(curr_chrom, file=sys.stderr)
 
 				overlap_indicies = rangeBsearch(frag['start'], frag['end'], 
 						gene_elements[curr_chrom][frag['strand']])
@@ -305,129 +296,25 @@ for bam in bamfiles:
 
 
 	bam_fp.close()
+			#parsedData[chrom][strand].append({
+			#		'gene_id':gene_id,
+			#		'start':start,
+			#		'end':end,
+			#		'exons':list(),
+			#		'intronic':list(),
+			#		'reads':list()
+			#	})
 
+outfile = open(outfilename, 'w')
 
-"""
-bam_fp = pysam.AlignmentFile(bamfile, "rb")
+outfile.write('chr\tstart\tend\tstrand\tgene_id\tcount\n')
 
-print("DEBUG: checking BAM index", file=sys.stderr)
-if not bam_fp.check_index():
-	print("ERROR: no index!", file=sys.stderr)
-	sys.exit(1)
+#FIXME handle multiple counts for different bamfiles!
+for chrom in gene_elements:
+	for strand in gene_elements[chrom]:
+		for gene in gene_elements[chrom][strand]:
+			outstr = chrom + '\t' + str(gene['start']) + '\t' + str(gene['end']) + '\t' + strand + '\t' + gene['gene_id'] + '\t' + str(len(gene['reads'])) + '\n'
+			outfile.write(outstr)
 
-
-print("DEBUG: starting to iterate over genes", file=sys.stderr)
-for gene in gene_elements:
-	print("DEBUG: gene is", gene, file=sys.stderr)
-	overlap_reads = dict()
-
-	for read in bam_fp.fetch(gene_elements[gene]['chrom'], gene_elements[gene]['start'], gene_elements[gene]['end']):
-		if ( read.is_secondary or 
-			read.is_duplicate or 
-			read.is_qcfail or 
-			read.is_unmapped or 
-			read.mate_is_unmapped or  
-			not read.is_paired or 
-			not read.is_proper_pair ):
-			continue
-
-		if read.query_name not in overlap_reads:
-			overlap_reads[read.query_name] = list()
-
-		overlap_reads[read.query_name].append(read)
-	
-	print("DEBUG: overlap_reads num:", len(overlap_reads), file=sys.stderr)
-	flag = False
-	for qname in overlap_reads:
-#		print(qname)
-#		for read in overlap_reads[qname]:
-#			print("\tChecking a read:")
-#			print("\t\tread1?:", read.is_read1)
-#			print("\t\tread2?:", read.is_read2)
-		if len(overlap_reads[qname]) != 2:
-			flag = True
-			print("Gene:", gene)
-			print("Overlap_reads = ", len(overlap_reads[qname]))
-			for read in overlap_reads[qname]:
-				print("\tChecking a read:")
-				print("\t\tread1?:", read.is_read1)
-				print("\t\tread2?:", read.is_read2)
-				mate_read = bam_fp.mate(read)
-				print("\t\tmate:", mate_read)
-
-				if ( mate_read.is_secondary or 
-					mate_read.is_duplicate or 
-					mate_read.is_qcfail or 
-					mate_read.is_unmapped or 
-					mate_read.mate_is_unmapped or  
-					not mate_read.is_paired or 
-					not mate_read.is_proper_pair ):
-					print("\t\t\tmate fails QC:")
-
-					if mate_read.is_secondary:
-						print("\t\t\t\tis_secondary")
-					elif mate_read.is_duplicate:
-						print("\t\t\t\tis_duplicate")
-					elif mate_read.is_qcfail:
-						print("\t\t\t\tis_qcfail")
-					elif mate_read.is_unmapped:
-						print("\t\t\t\tis_unmapped")
-					elif mate_read.mate_is_unmapped:
-						print("\t\t\t\tmate_is_unmapped")
-					elif not mate_read.is_paired:
-						print("\t\t\t\tNOT is_paired")
-					elif not mate_read.is_proper_paired:
-						print("\t\t\t\tNOT is_proper_paired")
-					else:
-						print("\t\t\t\tSomething else?")
-
-				else:
-					print("\t\t\tmate passes QC")
-					print("\t\t\tread aligned to:", read.get_reference_positions())
-					print("\t\t\tmate aligned to:", mate_read.get_reference_positions())
-					print("\t\t\tgene positions: chr", gene_elements[gene]['chrom'], "start", gene_elements[gene]['start'], "end", gene_elements[gene]['end'])
-					print("\t\t\tread overlap is:", read.get_overlap(gene_elements[gene]['start'], gene_elements[gene]['end']))
-					print("\t\t\tmate overlap is:", mate_read.get_overlap(gene_elements[gene]['start'], gene_elements[gene]['end']))
-					
-
-
-	print("DEBUG: done with gene", gene, file=sys.stderr)
-
-	if flag:
-		bam_fp.close()
-		sys.exit(1)
-bam_fp.close()
-
-#		print(line)
-#	line_list = line.split('\t')
-#	read_name = line_list[0]
-#	read_flag = line_list[1]
-#	read_start_pos = line_list[3]
-#	read_rnext = line_list[6]
-#	read_pnext = line_list[7]
-
-#	if (
-#		(not read_flag & 0x2) or    # unmapped
-#		(read_flag & 0x100) or      # multimapped
-#		(read_flag & 0x200) or      # low quality
-#		(read_flag & 400) or        # PCR duplicate
-#		(not read_flag & 0x1)       # unpaired
-#	): 
-#		continue
-	
-		if feature == 'gene':
-			parsedData[gene_id] = {
-					'chrom':chrom,
-					'start':start,
-					'end':end,
-					'strand':strand,
-					'exons':list(),
-					'intronic':list(),
-					'reads':list()
-					}
-		elif feature == 'exon':
-			parsedData[gene_id]['exons'].append({
-					'start':start,
-					'end':end
-					})
-"""
+outfile.close()
+			
